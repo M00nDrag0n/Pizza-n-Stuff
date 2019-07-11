eb39834d3f2d258a903ec42cb82b66227658095d
Student <example@example.com>
2019-07-10 20:40:20 -0700

    save the malfunctions

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 function getSelectedOptions(sel, fn) {
     var opts = [], opt;
 
+
+
     for (var i=0; len=sel.options.length; i<len; i++) {
         opt = sel.options[i];
 
@@ -12,10 +14,22 @@ function getSelectedOptions(sel, fn) {
             fn(opt);
           }
         }
+        return opt;
+      }
 
- function total(); {
+  function calculateAmount(opt) {
+    var total1 = 0;
+    var total2 = 0;
+    var total3 = 0;
+    var total4 = 0;
+    var grndTotal = [total1 + total2 + total3 + total4];
+  }
+  grndTotal = parseInt (total4) + parseInt(total3) + parseInt(total2) + parseInt(total1);
+   document.getSelectedOptions("Total").value = "$"+grndTotal ;
 
-   
+ whenClick.Order(alert);
+  alert: "Your pizza has been ordered!";
+
  }
     }
 
